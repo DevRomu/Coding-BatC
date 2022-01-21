c5e3e9cf613857c24f8f394f2b980902fa2e71af
Ro <orinarhomill@gmail.com>
2022-01-21 17:30:05 -0600

    Updated Solutions
    This is an if statement solution

--- Program.cs
+++ Program.cs
@@ -15,8 +15,36 @@ namespace Coding_BatSolution
     {
         static void Main(string[] args)
         {
+            int FirstNumber;
+            int SecondNumber;
+
+            Console.Write("\n\n");
+            Console.Write("Check whether two integers are equal or not:\n");
+            Console.Write("-------------------------------------------");
+            Console.Write("\n\n");
+
+
+            Console.Write("Input 1st number: ");
             ///I will try to find the solution to the If statement.
-            System.Console.WriteLine("Write the code here."); 
+            FirstNumber = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Input 2nd number: ");
+            ///Parseing intger to numerial numbers.
+            SecondNumber = Convert.ToInt32(Console.ReadLine());
+
+            /// <summary>
+            /// If the first number equals the second number, they will match.
+            /// or else they wouldn't match. 
+            /// </summary>
+            ///<param>First and Second number parameters.</param>
+            if (FirstNumber == SecondNumber)
+            {
+                Console.WriteLine("They Match!");
+            }
+            else
+            {
+                Console.WriteLine("They do not Match!");
+            }    
         }
     }
 }
