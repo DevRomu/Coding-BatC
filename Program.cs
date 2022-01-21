5394365310f88d08e5cc6e39395033abf322f870
Ro <orinarhomill@gmail.com>
2022-01-21 17:14:42 -0600

    C# Programming CodingSolutions
    This is where I do C# coding practice, while I use java for CodingBat

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+* Author: Rhomill Orina
+* Description: Testing Conditional Statements with If statements.
+*
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_BatSolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///I will try to find the solution to the If statement.
+            System.Console.WriteLine("Write the code here."); 
+        }
+    }
+}
