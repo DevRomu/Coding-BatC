9982503deeb11917c1122769f0311196e89e68d7
Ro <orinarhomill@gmail.com>
2022-01-21 21:40:46 -0600

    Editing Problem Statement
    I'll be editing the problem statements.

--- Problem2.cs
+++ Problem2.cs
@@ -1,8 +1,27 @@
-﻿using System;
+﻿/*
+* Author: Rhomill Orina
+* Description: Testing Conditional Statements with If statements.
+* Version: 1.0
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Class1
+namespace Coding_BatSolution
 {
-	public Class1()
+	public class Problem2
 	{
+		public Class1()
+		{
+			int FirstNumber;
+			int RemainderNumber;
+
+			Console.Write("\n\n")
+			Console
+
+		}
 	}
 }
+
