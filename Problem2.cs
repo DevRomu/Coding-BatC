bfd261cf50c23c020a086994f4ffd61b566cdf1b
Ro <orinarhomill@gmail.com>
2022-01-21 22:04:03 -0600

    Update Problem2.cs

--- Problem2.cs
+++ Problem2.cs
@@ -11,16 +11,24 @@ using System.Threading.Tasks;
 
 namespace Coding_BatSolution
 {
+	/// <summary>
+    /// The This will figure out if the number is even or odd.
+    /// </summary>
 	public class Problem2
 	{
+		/// <summary>
+        /// This Class contains the problem statement code
+        /// </summary>
 		public Class1()
 		{
 			int FirstNumber;
 			int RemainderNumber;
 
-			Console.Write("\n\n")
-			Console
-
+			Console.Write("/n/n")
+			Console.Write(">>/nThis program will check if the number is odd or even./n<<")
+			Console.Write("--------------------------------------------------------------")
+            Console.Write("Please Input an Integer:")
+			
 		}
 	}
 }
